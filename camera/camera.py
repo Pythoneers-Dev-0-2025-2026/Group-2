46885d809cdf1de302295a0ee179683efe2ab15b
AnnieR15 <annierabbani15@gmail.com>
2025-11-21 20:06:06 -0500

    Update camera.py

--- camera/camera.py
+++ camera/camera.py
@@ -2,14 +2,14 @@ import cv2
 cam = cv2.VideoCapture(0)
 
 if not cam.isOpened():
-    print("Error: Could not access camera.")
+    print("Error: Could not access camera")
     exit()
 
 while True:
     ret, frame = cam.read()
 
     if not ret:
-        print("Failed to grab frame.")
+        print("Failed to grab frame")
         break
 
     cv2.imshow("Camera Feed", frame)
