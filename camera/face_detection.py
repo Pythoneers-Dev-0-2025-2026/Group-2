1d6fef60a0ea489214d7bbb3c9b6bfb5865ce63b
Gurneik Boiteux <guboiteux@gmail.com>
2025-11-28 18:44:11 -0500

    Face detection implementation

---
+++ camera/face_detection.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+from camera import get_frame
+
+mp_face = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+face_detector = mp_face.FaceDetection(model_selection=0, min_detection_confidence=0.3)
+
+while True:
+    frame = get_frame()
+
+    if frame is None:
+        print("Could not grab frame")
+        break
+
+    #rgb conversion
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    faces = face_detector.process(img)
+
+    #boxes
+    if faces.detections:
+        for detection in faces.detections:
+            mp_drawing.draw_detection(frame, detection)
+
+    #display (just for me to test)
+    cv2.imshow("Face Detection", frame)
+
+    #to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
