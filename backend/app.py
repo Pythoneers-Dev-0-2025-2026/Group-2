53999bbfd44317d439f1d223fc218772df76622e
Arkaaz <arkaazattar@gmail.com>
2025-11-29 15:08:24 -0500

    basic commented definition of what lock will do, implementation later

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+import ctypes #provides windll functions
+import time
+
+
+def lock():
+    # if the camera sends a message that there is someone in frame, this needs to be called
+    # send a message to the phone asking if you want to lock the computer, if input is yes use ctypes lockwindows 
+    pass
+
+
+
+
+
+
