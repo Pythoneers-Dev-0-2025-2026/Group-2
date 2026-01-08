3dda93fb46d642a9a50e9357d1997ccce77253e7
Arkaaz <arkaazattar@gmail.com>
2026-01-08 15:56:02 -0500

    some structure, need to get wtsgetactivateconsolesession to work so that the login status can be accurately obtained

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,52 @@
 import ctypes #provides windll functions
 import time
 import socket
+import json
+import os
 
-hostname = socket.gethostname() # store laptop name is hostname
-ip = socket.gethostbyname(hostname)
-print(ip)
+# initialize as False
+lock_status = False
+
+request = { 
+    "lock_status": lock_status, 
+
+}
+
+data = json.dumps(request)
+
+# hostname = socket.gethostname() # store laptop name is hostname
+# ip = socket.gethostbyname(hostname)
+# print(ip)
 
 
-
-def lock():
-    
-    ## camera will detect something, call lock -> 
-
-    ## lock will run and send a message to the users phone asking if they want to lock ->
-
-    ## y -> lock
-
-    ## n -> return
-
+def lock():  
+ 
+    # kernel = ctypes.windll.
+    ##testing
+    status = bool(ctypes.windll.user32.OpenInputDesktop(0, False, 0x0100))
+    active_session_id = ctypes.windll.user32.WTSGetActiveConsoleSessionId()
+    print(f"Initial Status: {status} {active_session_id}")
     ctypes.windll.user32.LockWorkStation()
-    # this call locks the computer, and we want to lock user input as well
+    
+
+    loop = True
+    while loop:
+        if bool(ctypes.windll.user32.OpenInputDesktop(0, False, 0x0100)) == False:
+            time.sleep(2)
+            loop = False
+            # print(ctypes.windll.user32.WTSGetActiveConsoleSessionId())
+            print("Logged In")
+
+        # print(ctypes.windll.user32.WTSGetActiveConsoleSessionId())
+        print("Not Logged In")
+        time.sleep(1)
+    
 
 
 def proctor():
     pass
 
 
-
+lock()
 
 
