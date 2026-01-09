b63a5aeebd00b793eee45c77df9632b4204f757b
arkaazattar <arkaazattar@gmail.com>
2026-01-08 23:35:28 -0500

    functions to detect if user is locked out of workstation implemented.

--- backend/app.py
+++ backend/app.py
@@ -1,52 +1,64 @@
 import ctypes #provides windll functions
+import subprocess
 import time
-import socket
+import socket # will be used to send data between phone and laptop
 import json
 import os
 
-# initialize as False
-lock_status = False
 
-request = { 
-    "lock_status": lock_status, 
+wtsapi32 = ctypes.windll.wtsapi32
 
-}
+user32 = ctypes.windll.user32
+# print(user32)
 
-data = json.dumps(request)
+def lock():
+    """
+    Locks windows workstation, sets lock_status to True
+    """
+    user32.LockWorkStation()
+    check_lock_status()
+    return None
 
-# hostname = socket.gethostname() # store laptop name is hostname
-# ip = socket.gethostbyname(hostname)
-# print(ip)
+def check_lock_status():
+    """
+    Returns the current boolean value of a computers lock status.
+    Checks task manager for LogonUi.exe
+
+    Locked -> True
+    Unlocked -> False
+    """
+    output = subprocess.check_output('TASKLIST').decode()
+    if 'LogonUI.exe' in output:
+        return True
+    return False
+    
+# def proctor():
 
 
-def lock():  
- 
-    # kernel = ctypes.windll.
-    ##testing
-    status = bool(ctypes.windll.user32.OpenInputDesktop(0, False, 0x0100))
-    active_session_id = ctypes.windll.user32.WTSGetActiveConsoleSessionId()
-    print(f"Initial Status: {status} {active_session_id}")
-    ctypes.windll.user32.LockWorkStation()
+if __name__ == "__main__": 
+    
+    threat_detected = False # will change to the response of the camera
+
+    ## testing
+    lock()
+    
+    while True:
+
+        request = { 
+            # "threat_detection" : threat, needs to be realised from cv
+            "lock_status": check_lock_status(), 
+            "threat_detected" : threat_detected # this obviously will do nothing now
+        }
+        data = json.dumps(request)
+
+        if check_lock_status() == True:
+            print("Locked")
+        else:
+            print("Unlocked")
+        print(data)
+        time.sleep(0.5)
     
 
-    loop = True
-    while loop:
-        if bool(ctypes.windll.user32.OpenInputDesktop(0, False, 0x0100)) == False:
-            time.sleep(2)
-            loop = False
-            # print(ctypes.windll.user32.WTSGetActiveConsoleSessionId())
-            print("Logged In")
 
-        # print(ctypes.windll.user32.WTSGetActiveConsoleSessionId())
-        print("Not Logged In")
-        time.sleep(1)
-    
-
-
-def proctor():
-    pass
-
-
-lock()
 
 
