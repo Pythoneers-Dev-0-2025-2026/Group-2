d1bb833865708a63013ca44de6d13d0c71b76355
AnnieR15 <annierabbani15@gmail.com>
2026-01-11 01:09:12 -0500

    Update camera.py

--- camera/camera.py
+++ camera/camera.py
@@ -1,27 +1,38 @@
 import cv2
-cam = cv2.VideoCapture(0)
+import mediapipe as mp
+from .camera import get_frame #A
+from .camera import release_camera #A
+from .screenshots import save_and_send #A
 
-def get_frame():
-    ret, frame = cam.read()
-    if not ret:
-        return None
-    return frame
-if __name__ == "__main__":
-    if not cam.isOpened():
-        print("Error: Could not access camera")
-        exit()
 
-    while True:
-        frame = get_frame()
+mp_face = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
 
-        if frame is None:
-            print("Failed to grab frame")
-            break
+face_detector = mp_face.FaceDetection(model_selection=0, min_detection_confidence=0.3)
 
-        cv2.imshow("Camera Feed", frame)
+while True:
+    frame = get_frame()
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    if frame is None:
+        print("Could not grab frame")
+        break
 
-    cam.release()
-    cv2.destroyAllWindows()
+    #rgb conversion
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    faces = face_detector.process(img)
+
+    #boxes
+    if faces.detections:
+        for detection in faces.detections:
+            mp_drawing.draw_detection(frame, detection)
+            save_and_send(frame) #A
+
+    #display (just for me to test)
+    cv2.imshow("Face Detectionq", frame)
+
+    #to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        release_camera() #A
+        break
+
+cv2.destroyAllWindows()
